e5924a0880ac7cfb2a36b0b52b48d4324e4caca0
alexeysvyridov <alexeysvyridov@gmail.com>
2020-04-14 16:40:12 +0300

    delegate components

---
+++ src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, EventEmitter, Output, Injectable} from '@angular/core';
+
+@Component({
+  selector: 'app-cockpit',
+  templateUrl: './cockpit.component.html',
+  styleUrls: ['./cockpit.component.css']
+})
+export class CockpitComponent implements OnInit {
+  @Output() serverCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
+  @Output('bpCreated') blueprintCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
+  newServerName = '';
+  newServerContent = '';
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onAddServer(serverData: { serverName: string, serverContent: string }) {
+    this.serverCreated.emit({
+      serverName: this.newServerName,
+      serverContent: this.newServerContent
+    });
+  }
+
+  onAddBlueprint(blueprintData: { serverName: string, serverContent: string }) {
+    this.blueprintCreated.emit({
+      serverName: this.newServerName,
+      serverContent: this.newServerContent
+    });
+  }
+}
